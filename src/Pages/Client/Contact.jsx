6129802d495028cc22b4caf143ac4ab62de3289f
VannyMiniTouch <VannyMiniTouch@gmail.com>
2023-06-09 16:27:38 +0700

    remove Image

--- src/Pages/Client/Contact.jsx
+++ src/Pages/Client/Contact.jsx
@@ -6,8 +6,8 @@ import faq_vn from "../../contents/images/how_to_play_vn.png";
 const Contact = () => {
   return (
     <Fragment>
-      <img src={faq_en} alt="" />
-      <img src={faq_vn} alt="" />
+      {/* <img src={faq_en} alt="" />
+      <img src={faq_vn} alt="" /> */}
       <div>Contact</div>
     </Fragment>
   );
