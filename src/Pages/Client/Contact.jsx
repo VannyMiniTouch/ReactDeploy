e6ee1cdef2804242b6fcfef99b9937814d834b97
VannyMiniTouch <VannyMiniTouch@gmail.com>
2023-06-09 14:53:42 +0700

    move image to contact

--- src/Pages/Client/Contact.jsx
+++ src/Pages/Client/Contact.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { Fragment } from "react";
+
+import faq_en from "../../contents/images/how_to_play_en.png";
+import faq_vn from "../../contents/images/how_to_play_en.png";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <Fragment>
+      <img src={faq_en} alt="" />
+      <img src={faq_vn} alt="" />
+      <div>Contact</div>
+    </Fragment>
+  );
+};
 
-export default Contact
+export default Contact;
